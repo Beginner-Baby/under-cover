e2c27c0f6c2f5acd8bd59703b09234bbca3624c4
Beginner-Baby <rahul49shukla@gmail.com>
2025-01-23 20:45:58 +0530

    first edit

---
+++ undercover/lib/main.dart
@@ -0,0 +1,258 @@
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}*/
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const bro());
+}
+
+class bro extends StatelessWidget {
+  const bro({super.key});
+  @override
+  build(BuildContext context) {
+    return MaterialApp(
+        title: 'blue baby',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+                title: Center(
+                  child: Text('Happy Happy!!',
+                      style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(1000, 192, 1, 1))),
+                ),
+                backgroundColor: Color.fromARGB(1000, 76, 228, 109)),
+            body: mid()
+            ));
+  }
+}
+
+class mid extends StatelessWidget {
+  const mid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+        children:[
+          Transform.rotate(
+            angle:0.2,
+          child:Align(
+            alignment:Alignment(-0.7,0),
+          child :Stack( 
+            children :[
+                Text("Under",
+                    style: TextStyle(
+                      fontSize:50,
+                      fontWeight:FontWeight.bold,
+                      foreground : Paint()
+                      ..style=PaintingStyle.stroke
+                      ..strokeWidth =6
+                      ..color=Colors.black,
+                      )
+                ),
+                Text("Under",
+                    style: TextStyle(
+                      fontSize:50,
+                      fontWeight:FontWeight.bold,
+                      color: Color.fromARGB(1000,255, 208, 19)
+                                    )
+                    )
+            ]
+        )
+          ) 
+          ),
+        //this is for the cover
+          Transform.rotate(
+          angle:-0.2,
+          child:Align(
+          alignment:Alignment(0.7,0),
+          child: Stack(
+            children:[
+              Text(
+                  "Cover",
+                  style:TextStyle(
+                    fontSize:50,
+                    fontWeight:FontWeight.bold,
+                    foreground: Paint()
+                    ..style=PaintingStyle.stroke
+                    ..strokeWidth=6
+                    ..color=Colors.black,
+
+                  )
+                ),
+              Text(
+                  "Cover",
+                  style:TextStyle(
+                    fontSize:50,
+                    fontWeight:FontWeight.bold,
+                    color:Color.fromARGB(1000,255, 208, 19)
+                  )
+
+                ) 
+            ]
+          )
+        )
+            )
+      
+          //the rectangle where paly button lie 
+          ,Align(
+            alignment:Alignment(0,0.55),
+
+            child:Container(
+            width: 200,
+            height: 100,
+            decoration :BoxDecoration(
+              borderRadius:BorderRadius.circular(20),
+              color : Color.fromARGB(1000,255, 65, 65),
+            )
+          )  
+            )
+          //this the for the txt play 
+          ,Align(
+            alignment:Alignment(0,0.5),
+            child:Text("play",
+                      style:TextStyle(
+                          fontSize:60,
+                          fontWeight:FontWeight.bold,
+                          color:Color.fromARGB(1000,139, 245, 98),
+                        )
+                      )
+            )
+          //hey this is for the image 
+          ,Align(
+            alignment:Alignment(0,-0.8),
+            child:Container(
+              width:250,
+              height:250,
+              child:Image.asset('assets/image/undc_img.jpeg')
+            )
+          )
+          
+    ]
+      );
+  }
+}
