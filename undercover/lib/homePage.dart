149bfbb6727870084921e4221521b46298ec0152
Beginner-Baby <rahul49shukla@gmail.com>
2025-01-24 10:14:51 +0530

    link the home page with opeing page

--- undercover/lib/homePage.dart
+++ undercover/lib/homePage.dart
@@ -7,15 +7,19 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text("Unver Cover",
+            title: Text("Under Cover",
                 style: TextStyle(
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
                     color: Color.fromARGB(1000, 240, 0, 0))),
             backgroundColor: Color.fromARGB(255, 255, 184, 184)),
-        body: Stack(children:[
-          //add further code from here 
-        ]
-      ));
+        body: Stack(children: [
+          ElevatedButton(
+              onPressed: () {
+                print("jay shree krishna");
+              },
+              child: Text("is it working"))
+          //add further code from here
+        ]));
   }
 }
--- undercover/lib/main.dart
+++ undercover/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'homePage.dart';
 
 void main() {
   runApp(const bro());
@@ -9,20 +10,25 @@ class bro extends StatelessWidget {
   @override
   build(BuildContext context) {
     return MaterialApp(
-        title: 'blue baby',
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-                title: Center(
-                  child: Text('Happy Happy!!',
-                      style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(1000, 192, 1, 1))),
-                ),
-                backgroundColor: Color.fromARGB(1000, 76, 228, 109)),
-            body: mid()
-            ));
+        title: 'blue baby', debugShowCheckedModeBanner: false, home: opening());
+  }
+}
+
+class opening extends StatelessWidget {
+  const opening({super.key});
+  @override
+  build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Center(
+              child: Text('Happy Happy!!',
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(1000, 192, 1, 1))),
+            ),
+            backgroundColor: Color.fromARGB(1000, 76, 228, 109)),
+        body: mid());
   }
 }
 
@@ -31,103 +37,74 @@ class mid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-        children:[
-          Transform.rotate(
-            angle:0.2,
-          child:Align(
-            alignment:Alignment(-0.7,0),
-          child :Stack( 
-            children :[
+    return Stack(children: [
+      //this is for the text Under
+      Transform.rotate(
+          angle: 0.2,
+          child: Align(
+              alignment: Alignment(-0.7, 0),
+              child: Stack(children: [
                 Text("Under",
                     style: TextStyle(
-                      fontSize:50,
-                      fontWeight:FontWeight.bold,
-                      foreground : Paint()
-                      ..style=PaintingStyle.stroke
-                      ..strokeWidth =6
-                      ..color=Colors.black,
-                      )
-                ),
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 6
+                        ..color = Colors.black,
+                    )),
                 Text("Under",
                     style: TextStyle(
-                      fontSize:50,
-                      fontWeight:FontWeight.bold,
-                      color: Color.fromARGB(1000,255, 208, 19)
-                                    )
-                    )
-            ]
-        )
-          ) 
-          ),
-        //this is for the cover
-          Transform.rotate(
-          angle:-0.2,
-          child:Align(
-          alignment:Alignment(0.7,0),
-          child: Stack(
-            children:[
-              Text(
-                  "Cover",
-                  style:TextStyle(
-                    fontSize:50,
-                    fontWeight:FontWeight.bold,
-                    foreground: Paint()
-                    ..style=PaintingStyle.stroke
-                    ..strokeWidth=6
-                    ..color=Colors.black,
-
-                  )
-                ),
-              Text(
-                  "Cover",
-                  style:TextStyle(
-                    fontSize:50,
-                    fontWeight:FontWeight.bold,
-                    color:Color.fromARGB(1000,255, 208, 19)
-                  )
-
-                ) 
-            ]
-          )
-        )
-            )
-      
-          //the rectangle where paly button lie 
-          ,Align(
-            alignment:Alignment(0,0.55),
-
-            child:Container(
-            width: 200,
-            height: 100,
-            decoration :BoxDecoration(
-              borderRadius:BorderRadius.circular(20),
-              color : Color.fromARGB(1000,255, 65, 65),
-            )
-          )  
-            )
-          //this the for the txt play 
-          ,Align(
-            alignment:Alignment(0,0.5),
-            child:Text("play",
-                      style:TextStyle(
-                          fontSize:60,
-                          fontWeight:FontWeight.bold,
-                          color:Color.fromARGB(1000,139, 245, 98),
-                        )
-                      )
-            )
-          //hey this is for the image 
-          ,Align(
-            alignment:Alignment(0,-0.8),
-            child:Container(
-              width:250,
-              height:250,
-              child:Image.asset('assets/image/undc_img.jpeg')
-            )
-          )
-          
-    ]
-      );
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(1000, 255, 208, 19)))
+              ]))),
+      //this is for the cover
+      Transform.rotate(
+          angle: -0.2,
+          child: Align(
+              alignment: Alignment(0.7, 0),
+              child: Stack(children: [
+                Text("Cover",
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 6
+                        ..color = Colors.black,
+                    )),
+                Text("Cover",
+                    style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(1000, 255, 208, 19)))
+              ]))),
+      //the rectangle where paly button lie
+      Align(
+          alignment: Alignment(0, 0.55),
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MainPage())
+                    );
+                },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(1000, 255, 65, 65),
+              ),
+              child: Text("play",
+                  style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(1000, 139, 245, 98),
+                  )))),
+      //hey this is for the image
+      Align(
+          alignment: Alignment(0, -0.8),
+          child: Container(
+              width: 250,
+              height: 250,
+              child: Image.asset('assets/image/undc_img.jpeg')))
+    ]);
   }
 }
